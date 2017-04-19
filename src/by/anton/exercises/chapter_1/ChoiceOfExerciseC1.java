9349aa8b1d1b72b41dc5273c0cbaa063eb511cc1
Anton Buben <2900551@mail.ru>
2017-04-19 22:59:44 +0300

    Refactory

--- src/by/anton/exercises/chapter_1/ChoiceOfExerciseC1.java
+++ src/by/anton/exercises/chapter_1/ChoiceOfExerciseC1.java
@@ -17,11 +17,10 @@ public class ChoiceOfExerciseC1 {
     }
 
     public void setExercise() {
-        System.out.println("Enter Chapter 1..10:");
-        Scanner scan1 = new Scanner(System.in);
-        this.chapter = scan1.nextInt();
-        //this.chapter = 1;
-
+//        System.out.println("Enter Chapter 1..10:");
+//        Scanner scan1 = new Scanner(System.in);
+//        this.chapter = scan1.nextInt();
+//        //this.chapter = 1;
         System.out.println("Enter Exercise 1..10:");
         Scanner scan2 = new Scanner(System.in);
         this.exercise = scan2.nextInt();
