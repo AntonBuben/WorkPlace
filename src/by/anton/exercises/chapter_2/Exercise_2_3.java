b6f984a018d61d8305eacdf220cce0650524c34e
Anton Buben <2900551@mail.ru>
2017-04-23 22:43:07 +0300

    Chapter 2 3 done

--- src/by/anton/exercises/chapter_2/Exercise_2_3.java
+++ src/by/anton/exercises/chapter_2/Exercise_2_3.java
@@ -4,5 +4,29 @@ package by.anton.exercises.chapter_2;
  * Created by Buben_AV on 22.03.2017.
  */
 public class Exercise_2_3 {
-    private final String TASK = "#3 Ввести с консоли n целых чисел. На консоль вывести: Числа, которые делятся на 3 или на 9.";
+    private final String TASK = "#3 Вывести на консоль те числа, длина которых меньше (больше) средней,\n" +
+            "а также длину.";
+    private int arr[];
+
+    public Exercise_2_3() {
+        System.out.println(TASK);
+        ArrayForExercises arr = new ArrayForExercises();
+        this.arr = arr.getArray();
+        this.sortByMedium();
+    }
+    private void sortByMedium(){
+        int count[] = new int[this.arr.length];
+        int medium = 0;
+        for (int i = 0; i < this.arr.length; i++) {
+            count[i]=String.valueOf(Math.abs(this.arr[i])).length();
+            medium = medium + count[i];
+        }
+        medium = (int)(medium / count.length);
+        System.out.println("The numbers : ");
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] <= medium) {
+                System.out.println(this.arr[i]+"("+count[i]+") ");
+            }
+        }
+    }
 }
